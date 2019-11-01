fdfd873e28717f23963781c275284af9269d0db8
Keypasster <timbersell234@gmail.com>
2019-11-01 12:34:41 +0300

    Теперь всплывающая панель справки отображается над кнопкой, поверх остальных элементов. Исправили стили

--- include/js/main.js
+++ include/js/main.js
@@ -4,6 +4,9 @@ function showhelp() {
         help.setAttribute("style", "display:none");
     }
     else {
+	var help_button = document.getElementById("help_button");
         help.setAttribute("style", "display:block");
+	var top = help_button.offsetTop	- help.clientHeight - 50;
+	help.setAttribute("style", "display:block; top:" + top + "px");
     }
 }
